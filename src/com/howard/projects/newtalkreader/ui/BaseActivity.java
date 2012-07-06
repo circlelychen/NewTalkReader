78d390b7546f61efbef4f9fea473e4492982bb97
howard.hy_chen <truecirclely@gmail.com>
2012-07-06 19:11:35 +0800

    [master] create android project for NewTalk reader

---
+++ src/com/howard/projects/newtalkreader/ui/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.howard.projects.newtalkreader.ui;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+
+public abstract class BaseActivity extends SherlockFragmentActivity{
+	
+	
+}
---
+++ src/com/howard/projects/newtalkreader/ui/NewTalkChannelActivity.java
@@ -0,0 +1,16 @@
+package com.howard.projects.newtalkreader.ui;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.howard.projects.newtalkreader.R;
+
+public class NewTalkChannelActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_talk_channel);
+    }
+
+}

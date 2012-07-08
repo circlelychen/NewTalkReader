006e7efed401551b109fecce3d1d025a874cdf56
howard.hy_chen <truecirclely@gmail.com>
2012-07-09 01:10:04 +0800

    [AbNavigationList] Add navigation list in action bar

--- src/com/howard/projects/newtalkreader/ui/NewTalkChannelActivity.java
+++ src/com/howard/projects/newtalkreader/ui/NewTalkChannelActivity.java
@@ -1,16 +1,44 @@
 package com.howard.projects.newtalkreader.ui;
 
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.ArrayAdapter;
+import android.widget.SpinnerAdapter;
 
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.ActionBar.TabListener;
 import com.howard.projects.newtalkreader.R;
 
 public class NewTalkChannelActivity extends BaseActivity {
 
+	private SpinnerAdapter categoryAdaptr;
+	private boolean mDualPane;
+			
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_talk_channel);
+        		
+        mDualPane = false;
+        // Setup the Action Bar
+ 		ActionBar actionBar = getSupportActionBar();
+ 		actionBar.setDisplayShowTitleEnabled(false);
+ 		categoryAdaptr = new ArrayAdapter<String>(getApplication(),android.R.layout.simple_spinner_dropdown_item,this.getResources().getStringArray(R.array.rss_channel_category));
+ 		if (!mDualPane) {
+ 			actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+ 			actionBar.setListNavigationCallbacks(categoryAdaptr,
+ 					new ActionBar.OnNavigationListener() {
+ 						@Override
+ 						public boolean onNavigationItemSelected(
+ 								int itemPosition, long itemId) {
+// 							onSourceSelected((int) itemId);
+ 							return true;
+ 						}
+ 					});
+ 		} else {
+ 			actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+ 		}
     }
-
 }

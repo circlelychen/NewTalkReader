b2d9a56a3c459b492f5c1e7d2713111af83e7378
howard.hy_chen <truecirclely@gmail.com>
2012-07-10 20:24:19 +0800

    [AbNavigationList] add navigation list on actionbar to show two viewing choice.

--- src/com/howard/projects/newtalkreader/ui/NewTalkChannelActivity.java
+++ src/com/howard/projects/newtalkreader/ui/NewTalkChannelActivity.java
@@ -1,32 +1,43 @@
 package com.howard.projects.newtalkreader.ui;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
 import com.actionbarsherlock.app.ActionBar;
-import com.actionbarsherlock.app.ActionBar.Tab;
-import com.actionbarsherlock.app.ActionBar.TabListener;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.howard.projects.newtalkreader.R;
+import com.howard.projects.newtalkreader.ui.phone.ChannelsPagerFragment;
 
-public class NewTalkChannelActivity extends BaseActivity {
+public class NewTalkChannelActivity extends SherlockFragmentActivity {
 
+	private static final String LOG_TAG = NewTalkChannelActivity.class.getSimpleName();
 	private SpinnerAdapter categoryAdaptr;
 	private boolean mDualPane;
 			
+	private static final String SAVED_SELECTED_CATEGORY = "_s_s";
+	private static final String SAVED_SELECTED_CHANNEL = "_s_c";
+	
+	private int mSelectedCategory;
+	private String mSelectedChannel;
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_talk_channel);
+        setContentView(R.layout.newtalk_singlepane);
         		
         mDualPane = false;
         // Setup the Action Bar
  		ActionBar actionBar = getSupportActionBar();
  		actionBar.setDisplayShowTitleEnabled(false);
- 		categoryAdaptr = new ArrayAdapter<String>(getApplication(),android.R.layout.simple_spinner_dropdown_item,this.getResources().getStringArray(R.array.rss_channel_category));
+ 		categoryAdaptr = new ArrayAdapter<String>(getApplication(),
+ 				android.R.layout.simple_spinner_dropdown_item,
+ 				getResources().getStringArray(R.array.rss_channel_category));
  		if (!mDualPane) {
  			actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
  			actionBar.setListNavigationCallbacks(categoryAdaptr,
@@ -34,12 +45,84 @@ public class NewTalkChannelActivity extends BaseActivity {
  						@Override
  						public boolean onNavigationItemSelected(
  								int itemPosition, long itemId) {
- 							Toast.makeText(getApplication(), "Select Category " + (String)categoryAdaptr.getItem(itemPosition), Toast.LENGTH_SHORT).show();
+ 							onCategorySelected(itemPosition);
  							return true;
  						}
  					});
  		} else {
  			actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
  		}
+ 		
+ 		// Reload configuration parameter from SharedPreferece
+ 		SharedPreferences prefs = PreferenceManager.
+ 				getDefaultSharedPreferences(this);
+		mSelectedCategory = prefs.getInt("mSelectedCategory",0);
     }
+    
+    
+    
+    @Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		
+		
+		// Store the configyration parameter into SharedPreference
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		SharedPreferences.Editor editor = prefs.edit();
+		editor.putInt("mSelectedCategory", mSelectedCategory);
+		editor.commit();
+	}
+
+
+
+	protected void onResume(){
+    	super.onResume();
+    	getSupportActionBar().setSelectedNavigationItem(mSelectedCategory);
+    }
+    
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		Log.i(LOG_TAG, "onRestoreInstanceState()");
+		super.onRestoreInstanceState(savedInstanceState);
+		restoreSelection(savedInstanceState);	
+	}
+	
+	private void restoreSelection(Bundle savedInstanceState) {
+		if (savedInstanceState != null) {
+			mSelectedCategory = savedInstanceState.getInt(SAVED_SELECTED_CATEGORY);
+			mSelectedChannel = savedInstanceState.getString(SAVED_SELECTED_CHANNEL);
+		}
+		
+//		onChannelSelected(mSelectedChannel);
+	}
+    
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		Log.i(LOG_TAG, "onSaveInstanceState()");
+		super.onSaveInstanceState(outState);
+		outState.putInt(SAVED_SELECTED_CATEGORY, mSelectedCategory);
+		outState.putString(SAVED_SELECTED_CHANNEL, mSelectedChannel);
+	}
+    
+	
+	private void onCategorySelected(int category) {
+		Log.i(LOG_TAG,"Select Category on item position: " + category);
+		mSelectedCategory = category;
+//		SharedPreferencesUtils.putInt(this, SettingsActivity.PREF_LAST_SOURCE,
+//				mSelectedSource);
+		if (!mDualPane) {
+			ChannelsPagerFragment frag = (ChannelsPagerFragment) getSupportFragmentManager()
+					.findFragmentById(R.id.fragment_channels_items);
+//			frag.loadChannels(mSelectedCategory);
+//			onChannelSelected(mSelectedChannel);
+		} else {
+//			ChannelsListFragment frag = (ChannelsListFragment) getSupportFragmentManager()
+//					.findFragmentById(R.id.fragment_channels);
+//			frag.loadChannels(source);
+//			onChannelSelected(mSelectedChannel);
+		}
+	}
 }
+

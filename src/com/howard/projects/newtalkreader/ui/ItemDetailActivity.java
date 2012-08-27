b676a9a1ae6ba47908b5a0fb56424b41f503251b
howard.hy_chen <truecirclely@gmail.com>
2012-08-28 00:34:21 +0800

    implement onCreate() and onResume() for Activity: initial views and start loader task

--- src/com/howard/projects/newtalkreader/ui/ItemDetailActivity.java
+++ src/com/howard/projects/newtalkreader/ui/ItemDetailActivity.java
@@ -1,42 +1,70 @@
 package com.howard.projects.newtalkreader.ui;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.Loader;
+import android.text.Html;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
+import com.google.android.imageloader.ImageLoader;
 import com.howard.projects.newtalkreader.R;
 import com.howard.projects.newtalkreader.utils.DLog;
 
 public class ItemDetailActivity extends SherlockFragmentActivity implements
-		LoaderManager.LoaderCallbacks<Cursor>{
+		LoaderManager.LoaderCallbacks<NewsEntity>{
 
 	private static String TAG = ItemDetailActivity.class.getSimpleName();
 	private Uri mItemUri; 
 	
+	private TextView mTitle;
+	private TextView mDescription;
+	private ImageView mImage;
 	private View mLoading;
 	private View mError;
+	private ImageLoader mImageLoader;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.newtalk_item_detail);
-		
 		Bundle bundle = this.getIntent().getExtras();
-		this.setTitle(bundle.getString("_title"));
 		mItemUri = bundle.getParcelable("_link");
+		
+		setContentView(R.layout.newtalk_item_detail);
+		mTitle = (TextView) this.findViewById(R.id.title);
+		mDescription = (TextView) this.findViewById(R.id.description);
+		mImage = (ImageView) this.findViewById(R.id.image);
+		mLoading = (View) this.findViewById(R.id.loading);
+		mError = (View) this.findViewById(R.id.error);
+		
+		mImageLoader = new ImageLoader();
 	}
 
 	@Override
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
+		
+		this.getSupportLoaderManager().initLoader(mItemUri.hashCode(), 
+				Bundle.EMPTY, 
+				this).forceLoad();
+				
 	}
 
 	@Override

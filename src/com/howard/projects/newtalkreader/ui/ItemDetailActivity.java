c0b418faac35c9203987da07504116a120b8a52d
howard.hy_chen <truecirclely@gmail.com>
2012-08-28 00:35:48 +0800

    add NewsEntity and use it to define AsyncTaskLoader to load news content

--- src/com/howard/projects/newtalkreader/ui/ItemDetailActivity.java
+++ src/com/howard/projects/newtalkreader/ui/ItemDetailActivity.java
@@ -81,24 +81,114 @@ public class ItemDetailActivity extends SherlockFragmentActivity implements
 	
 	
 	@Override
-	public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
+	public Loader<NewsEntity> onCreateLoader(int arg0, Bundle arg1) {
 		// TODO Auto-generated method stub
 		DLog.d(TAG,"create Loader on " + mItemUri);
-		return null;
+		
+		return new NewsLoader(this, mItemUri);
 	}
 
 	@Override
-	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+	public void onLoadFinished(Loader<NewsEntity> loader, NewsEntity data) {
 		// TODO Auto-generated method stub
 		DLog.d(TAG,"Loader finished on " + mItemUri);  
 		
+		Bundle bundle = this.getIntent().getExtras();
+		mTitle.setText(bundle.getString("_title"));
+		
+		DLog.i(TAG,data.toString());
+		mDescription.setText(Html.fromHtml(data.content));
+		if(data.imgUrl == Uri.EMPTY){
+			mImage.setImageBitmap(null);
+			mImage.setVisibility(View.GONE);
+		}else{
+			mImageLoader.bind(mImage, data.imgUrl.toString(), null);
+			mImage.setVisibility(View.VISIBLE);
+		}
+		
+		
+		mTitle.setVisibility(View.VISIBLE);
+		mDescription.setVisibility(View.VISIBLE);
 		mLoading.setVisibility(View.GONE);
+		mError.setVisibility(View.GONE);
 		//mError.setVisibility(mAdapter.isEmpty() && mAdapter.hasError() ? View.VISIBLE : View.GONE);
 	}
 
 	@Override
-	public void onLoaderReset(Loader<Cursor> arg0) {
+	public void onLoaderReset(Loader<NewsEntity> arg0) {
 		// TODO Auto-generated method stub
-		//mAdapter.swapCursor(null);
+	}
+}
+
+class NewsEntity {
+	public Uri imgUrl; 
+	public String content;
+	public NewsEntity(){
+		reset();
+	}
+	
+	public void reset(){
+		//reference to resource assets in term of Uri form
+		this.imgUrl = Uri.EMPTY;
+		this.content = "";
+	}
+	
+	public String toString(){
+		return "NewsEntity is [ imgUrl: " + this.imgUrl + " ] and [ content: " + this.content + " ]";
+	}
+}
+
+class NewsLoader extends AsyncTaskLoader<NewsEntity>{
+
+	private static String TAG = NewsLoader.class.getSimpleName();
+	private Uri mUrl;
+	
+	public NewsLoader(Context context, Uri url){
+		super(context);
+		mUrl = url;
+	}
+	
+	public NewsLoader(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public NewsEntity loadInBackground() {
+		// TODO Auto-generated method stub
+		DLog.d(TAG,"loadInBackground()");
+		NewsEntity entity = new NewsEntity();
+		
+		Document doc;
+		try {
+			doc = Jsoup.connect(mUrl.toString()).get();
+		}catch (IOException e) {
+			DLog.e(TAG, "IOException Exception ");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return entity;
+		}
+			
+		//parse and fetch thumbnail url
+		try{
+			Element imageElement = doc.select("div.news_ctxt_area_pic a img").first();
+			DLog.i(TAG, "select div.news_ctxt_area_pic a img :" + imageElement.attr("src"));
+			entity.imgUrl = Uri.parse(imageElement.attr("src"));
+		}catch(NullPointerException e){
+			DLog.e(TAG, "NullPointer Exception : parse and fetch thumbnail url");
+			e.printStackTrace();
+		}
+		
+		//parse and fetch text coontent
+		try{
+			Element textElement = doc.select("div.news_ctxt_area_word").first();
+			DLog.i(TAG, "select div.news_ctxt_area_word : " + textElement.text());
+			entity.content = new String(textElement.html());
+		}catch(NullPointerException e){
+			DLog.e(TAG, "NullPointer Exception : parse and fetch text coontent");
+			e.printStackTrace();
+		}
+		
+		return entity;
 	}
 }

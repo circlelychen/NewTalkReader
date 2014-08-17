99f45eda73cc5baea28a374f941cc8cc23c898f5
circlelychen <truecirclely@gmail.com>
2014-08-17 11:14:31 +0800

    remove promotion contnet in sharing funciton

--- src/com/howard/projects/newtalkreader/ui/ItemDetailActivity.java
+++ src/com/howard/projects/newtalkreader/ui/ItemDetailActivity.java
@@ -81,11 +81,11 @@ public class ItemDetailActivity extends SherlockFragmentActivity implements
 		buf.append(this.mTitleValue)
 				.append(System.getProperty("line.separator"))
 				.append(this.mItemUri.toString())
-				.append(System.getProperty("line.separator"))
-				.append(System.getProperty("line.separator"))
-				.append("shared by ")
-				.append(this.getResources().getString(
-						R.string.newtalk_promote_appname));
+				.append(System.getProperty("line.separator"));
+//				.append(System.getProperty("line.separator"))
+//				.append("shared by ")
+//				.append(this.getResources().getString(
+//						R.string.newtalk_promote_appname));
 		intent.putExtra(Intent.EXTRA_TEXT, buf.toString());
 		
 		this.mShareActionProvider = (ShareActionProvider) menu.findItem(R.id.menu_share).getActionProvider();
